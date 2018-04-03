728fa872dcb676b0053c9d2fab618307251b94e7
Anthony Le Cigne <dev@lecigne.net>
2018-04-03 14:54:05 +0200

    Ajout de clients standards dans méthode main

---
+++ src/fr/proxibanquesi/tests/DummyClients.java
@@ -0,0 +1,21 @@
+package fr.proxibanquesi.tests;
+
+import fr.proxibanquesi.dao.ClientDao;
+import fr.proxibanquesi.dao.ClientDaoImp;
+import fr.proxibanquesi.model.Client;
+
+public class DummyClients {
+
+	public static void main(String[] args) {
+		ClientDao clientDao = new ClientDaoImp();
+
+		Client c1 = new Client("Dupont", "Michel", "1 rue 1", "75000", "Paris", "0100000000");
+		Client c2 = new Client("Durand", "Michel", "1 rue 2", "75000", "Paris", "0100000000");
+		Client c3 = new Client("Martin", "Michel", "1 rue 3", "75000", "Paris", "0100000000");
+
+		clientDao.creerClient(c1);
+		clientDao.creerClient(c2);
+		clientDao.creerClient(c3);
+	}
+
+}
