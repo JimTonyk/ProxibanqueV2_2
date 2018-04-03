24ca0cdaa44f6d36fabd7b609dea60733c527053
Anthony Le Cigne <dev@lecigne.net>
2018-04-03 16:58:02 +0200

    Ajout d'un login de base

---
+++ src/fr/proxibanquesi/presentation/Login.java
@@ -0,0 +1,56 @@
+package fr.proxibanquesi.presentation;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+
+	private static final String validPassword = "proxibanque";
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String password = request.getParameter("password");
+
+		RequestDispatcher dispatcher;
+
+		if (validPassword.equalsIgnoreCase(password)) {
+			dispatcher = request.getRequestDispatcher("index.html");
+		} else {
+			dispatcher = request.getRequestDispatcher("login.html");
+		}
+		dispatcher.forward(request, response);
+	}
+}
